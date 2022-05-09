daba19098ea01e4007d1a640e2d7d1e3082e0805
alfie.sharp@aveva.com <alfie.sharp@aveva.com>
2022-05-09 09:24:28 +0100

    Guessr

--- Guessr.js
+++ Guessr.js
@@ -2,68 +2,93 @@ const fs = require("fs");
 const is_number = require("is-number");
 const readline = require("readline");
 
-function askQuestion(query) {
-	const rl = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout,
-	});
-	return new Promise((resolve) =>
-		rl.question(query, (ans) => {
-			rl.close();
-			resolve(ans);
-		})
-	);
-}
-let userScore;
-let continueGame;
-let userNum;
-let systemNum;
-async function game() {
-	for (i = 0; i != 1; ) {
-		systemNum = Math.floor(Math.random() * 5) + 1;
-		for (k = 0; k != 1; ) {
-			userNum = Math.floor(await askQuestion("Enter number: "));
-			if (is_number(userNum) && userNum > 0 && userNum < 7) {
-				k = 1;
+class GuessrGame {
+	userScore;
+	continueGame;
+	userNum;
+	systemNum;
+	userName;
+	regex = /[/\\?%*:|"<>]/g;
+	regResult;
+
+	ask(query) {
+		const rl = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout,
+		});
+		return new Promise((resolve) =>
+			rl.question(query, (ans) => {
+				rl.close();
+				resolve(ans);
+			})
+		);
+	}
+
+	async game() {
+
+		for (let l = 0; l != 1; ) {
+			this.userName = await this.ask("Enter Username: ");
+			this.userName.toLowerCase();
+			this.regResult = this.userName.match(this.regex);
+			if ((!Array.isArray(this.regResult))) {
+				l = 1;
 			}
 		}
-		if (fs.existsSync("./Guessrdata.txt")) {
-			userScore = parseInt(
-				fs.readFileSync("./Guessrdata.txt", {
-					encoding: "utf8",
-					flag: "r",
-				})
-			);
-		} else {
-			userScore = 0;
-		}
-		switch (userNum) {
-			case systemNum:
-				userScore += 2;
-				break;
-			case systemNum - 1:
-			case systemNum + 1:
-				userScore++;
-				break;
-			default:
-				break;
-		}
-		fs.writeFileSync("./Guessrdata.txt", "" + userScore + "");
-		console.log(`System Number: ${systemNum}`);
-		console.log(`User Number: ${userNum}`);
-		console.log(`User Score: ${userScore}`);
-		for (j = 0; j != 1; ) {
-			continueGame = await askQuestion("Continue? (y/n) ");
-			if (continueGame === "n") {
-				i = 1;
-				j = 1;
-			} else if (continueGame === "y") {
-				i = 0;
-				j = 1;
+
+		for (let i; i != 1; ) {
+			this.systemNum = Math.floor(Math.random() * 5) + 1;
+			for (let k; k != 1; ) {
+				this.userNum = Math.floor(await this.ask("Enter number: "));
+				if (
+					is_number(this.userNum) &&
+					this.userNum > 0 &&
+					this.userNum < 7
+				) {
+					k = 1;
+				}
+			}
+			if (fs.existsSync(`./Guessrdata${this.userName}.txt`)) {
+				this.userScore = parseInt(
+					fs.readFileSync(`./Guessrdata${this.userName}.txt`, {
+						encoding: "utf8",
+						flag: "r",
+					})
+				);
 			} else {
-				continueGame = null;
+				this.userScore = 0;
+			}
+			switch (this.userNum) {
+				case this.systemNum:
+					this.userScore += 2;
+					break;
+				case this.systemNum - 1:
+				case this.systemNum + 1:
+					this.userScore++;
+					break;
+				default:
+					break;
+			}
+			fs.writeFileSync(
+				`./Guessrdata${this.userName}.txt`,
+				"" + this.userScore + ""
+			);
+			console.log(`System Number: ${this.systemNum}`);
+			console.log(`User Number: ${this.userNum}`);
+			console.log(`User Score: ${this.userScore}`);
+			for (let j; j != 1; ) {
+				this.continueGame = await this.ask("Continue? (y/n) ");
+				if (this.continueGame === "n") {
+					i = 1;
+					j = 1;
+				} else if (this.continueGame === "y") {
+					j = 1;
+				} else {
+					this.continueGame = null;
+				}
 			}
 		}
 	}
 }
-game();
+let testGame = new GuessrGame();
+testGame.game();
+module.exports = GuessrGame;
---
+++ Guessr.test.js
@@ -0,0 +1,48 @@
+const GuessrGame = require("./Guessr.js")
+
+let testGame = new GuessrGame();
+
+test("", async () => {
+    const game = await testGame.game();
+    expect(game).toBe(1)
+})
+
+// test("", () => {
+//     expect().toBe(1)
+// })
+
+// test("", () => {
+//     expect().toBe(1)
+// })
+
+// test("", () => {
+//     expect().toBe(1)
+// })
+
+// test("", () => {
+//     expect().toBe(1)
+// })
+
+// test("", () => {
+//     expect().toBe(1)
+// })
+
+// test("", () => {
+//     expect().toBe(1)
+// })
+
+// test("", () => {
+//     expect().toBe(1)
+// })
+
+// test("", () => {
+//     expect().toBe(1)
+// })
+
+// test("", () => {
+//     expect().toBe(1)
+// })
+
+// test("", () => {
+//     expect().toBe(1)
+// })
